c20e1f01ff28e00d8774e32e40acf33f9f98a272
Nicita01 <nicitamac@gmail.com>
2017-11-04 14:50:48 +0200

    +DB, +selectLanguage, +hint.json

--- DBMS/DBMS.js
+++ DBMS/DBMS.js
@@ -1,1 +1,33 @@
 'use strict';
+
+const fs = require('fs');
+const rl = require('readline');
+
+const inter = rl.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+let hintNumber;
+let language;
+let dir = __dirname
+selectLanguage()
+
+function selectLanguage(){
+  inter.question('Select the language: en/ru\n', function(choiseLanguage){
+  language = choiseLanguage === 'en' ? (() => {hintNumber = 1; return 0})() :
+                 choiseLanguage === 'ru' ? (() => {hintNumber = 1; return 1})() :
+                 (() => {hintNumber = 0; selectLanguage()})();
+  showHint(hintNumber);
+  setTimeout(onStart(12), 2000)
+  });
+};
+
+const hint = fs.readFileSync(dir + '/hints.json');
+const hintParse = JSON.parse("" + hint);
+function showHint(hintNumber){
+  hintNumber === 0 ? () => {} : inter.write(hintParse[language][hintNumber]);
+};
+function fff(){console.log(language); rl.pause};
+function onStart(dir){
+  showHint(2)
+};
