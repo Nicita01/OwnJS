2b14709cd334aeb5c0aa1e8fc99b71b6323de609
Nicita01 <31672085+Nicita01@users.noreply.github.com>
2017-11-06 20:47:19 +0200

    + validAccess
    add validation of permission to access the tables

---
+++ DBMS/validAccess.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function validAccess(controlAccessNumber, userAccessNumber){
+  let access = false;
+  for (let i = 1; i <= controlAccessNumber; i = i * 2 ){
+    if (userAccessNumber % (i * 2) === 0){
+       continue;
+    } else if (controlAccessNumber === i) {
+      access = true;
+    } else {
+        userAccessNumber -= i;
+      };
+    };
+  return access;
+  };
+console.log(validAccess(4,7));
