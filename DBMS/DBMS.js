b2c104c0aad9702d29f5ec079ad459afabeeafca
Nicita01 <31672085+Nicita01@users.noreply.github.com>
2017-11-06 03:59:16 +0200

    Fixed DBMS until active DB

--- DBMS/DBMS.js
+++ DBMS/DBMS.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 const rl = require('readline');
+const tt = require('./SQLtoJS.js');
 
 const inter = rl.createInterface({
   input: process.stdin,
@@ -18,24 +19,28 @@ function selectLanguage(){
              choiseLanguage === 'ru' ? (() => {hintNumber = 1; return 1})() :
              (() => {hintNumber = 0; selectLanguage()})();
   showHint(hintNumber);
-
   language === undefined ? () => {} : setTimeout(onStart, 1000);
   });
 };
 
 const hint = fs.readFileSync(dir + '/hints.json');
 const hintParse = JSON.parse("" + hint);
-function showHint(hintNumb){
-  hintNumb === 0 ? () => {} : inter.write(hintParse[language][hintNumb]);
+function showHint(hintNumber){
+  hintNumber === 0 ? () => {} : inter.write(hintParse[language][hintNumber]);
 };
 
 function onStart(){
   showHint(2);
-  fs.access(dir + '/DataBase.json', fs.constants.F_OK, (err) => {
-    !err ? (() => {
-      showHint(3);
-      console.log('\x1b[35m' + ' ' + dir + '/DataBase.json')
-    })() : console.log(12);
-  });
-
+  setTimeout(
+    () => fs.access(dir + '/DataBase.json', fs.constants.F_OK, (err) => {
+      !err ? (() => {
+        showHint(3);
+        inter.write('\x1b[36m' + ' ' + dir + '/DataBase.json\n' + '\x1b[37m');
+        showHint(5);
+      })() : (() => {
+        inter.write('\x1b[31m');
+        showHint(4);
+      })()
+    }), 1500);
 };
+
