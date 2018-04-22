a85f77cd47f06696c33e0ae6a7d5ed3be00d7d03
Nicita01 <nicitamac@gmail.com>
2018-04-22 19:04:34 +0300

    Refactor

--- A*/graph-maker.js
+++ A*/graph-maker.js
@@ -1,35 +1,5 @@
 'use strict';
 
-let rl = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-let dataStrArr = require('fs')
-.readFileSync(__dirname + '/input.txt')
-.toString()
-.split('\n');
-
-console.log('  ABCDEFGH'); // TODO: mode letters
-for (let curString in dataStrArr) {
-  console.log(curString, dataStrArr[curString].replace(/ /g, String.fromCharCode(45)));
-}
-
-const dataArr = dataStrArr.map(curStr => curStr.split(''));
-console.log(dataArr)
-
-// TODO: валидация входных данных и вводимых знаечний
-
-rl.question('Введите координаты начала пути (в формате A0):', (start) => {
-  rl.question('Введите координаты цели:', (finish) => {
-    let startX = start.charCodeAt() - 'A'.charCodeAt();
-    let startY = parseInt(start[1])
-    let finishX = finish.charCodeAt() - 'A'.charCodeAt();
-    let finishY = parseInt(finish[1])
-    makeGraph(dataStrArr, startX, startY, finishX, finishY);
-    rl.close();
-  })
-})
 const arrArrsNodes = [] // [[{}, {}], [{}, {}]]
 
 function GrateGraph(curString, curColumn, start = false, finish = false) {
@@ -37,9 +7,7 @@ function GrateGraph(curString, curColumn, start = false, finish = false) {
   this.down = null;
   this.left = null;
   this.right = null;
-  // let up = arrArrsNodes[curString - 1][curColumn]; 
   if (curString != 0 && arrArrsNodes[curString - 1][curColumn]) {
-    console.log('curString', curString)
     this.up = arrArrsNodes[curString - 1][curColumn];
     arrArrsNodes[curString - 1][curColumn].down = this;
   }
@@ -54,8 +22,11 @@ function GrateGraph(curString, curColumn, start = false, finish = false) {
   this.ifFinish = finish;
 }
 
-function makeGraph(dataStrArr, startX, startY, finishX, finishY) {
-  console.log(startX, startY, finishX, finishY);
+function makeGraph(dataArr, start, finish) {
+  let startX = start.charCodeAt() - 'A'.charCodeAt();
+  let startY = parseInt(start[1])
+  let finishX = finish.charCodeAt() - 'A'.charCodeAt();
+  let finishY = parseInt(finish[1])
   for (let curString in dataArr) {
     arrArrsNodes[curString] = [];
     for (let curColumn in dataArr[curString]) {
@@ -70,7 +41,7 @@ function makeGraph(dataStrArr, startX, startY, finishX, finishY) {
       );
     }
   }
-  console.dir(arrArrsNodes, {depth: null})
-   /* OTL: */ console.log(arrArrsNodes[1][1].up.down.up.down.up.down); // OK
-   module.exports = arrArrsNodes[startX, startY];
+  return arrArrsNodes[startX, startY];
 }
+
+module.exports = makeGraph
--- A*/index.js
+++ A*/index.js
@@ -1,1 +1,31 @@
-console.log(require(__dirname + '/graph-maker.js'))
+'use strict';
+
+const makeGraph = require(__dirname + '/graph-maker');
+
+let rl = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let dataStrArr = require('fs')
+.readFileSync(__dirname + '/input.txt')
+.toString()
+.split('\n');
+
+console.log('  ABCDEFGH'); // TODO: more letters
+for (let curString in dataStrArr) {
+  console.log(curString, dataStrArr[curString].replace(/ /g, String.fromCharCode(45)));
+}
+
+const dataArr = dataStrArr.map(curStr => curStr.split(''));
+console.log(dataArr)
+
+// TODO: валидация входных данных и вводимых знаечний
+
+rl.question('Введите координаты начала пути (в формате A0):', (start) => {
+  rl.question('Введите координаты цели:', (finish) => {
+    let starter = makeGraph(dataArr, start, finish);
+    rl.close();
+    console.log(starter)
+  })
+})
