c8fcb8db2586e2e6c4e223016f89e5e41b5c6986
Nicita01 <nicitamac@gmail.com>
2018-04-23 02:44:43 +0300

    Binary-heap is done

--- A*/binary-heap.js
+++ A*/binary-heap.js
@@ -7,9 +7,9 @@ function Heap() {
 }
 
 function Node(priority, data) {
-  this.parent = null;
   this.priority = priority;
   this.data = data;
+  this.parent = null;
   this.childLeft = null;
   this.childRight = null;
 }
@@ -19,7 +19,7 @@ Heap.prototype.getByIndex = function(index) {
     return this.root;
   }
   let curNode = this.root;
-  let curIndex = index; // Index of root
+  let curIndex = index;
   while (curIndex !== 2 && curIndex !== 3) {
     let lowerLine = Math.pow(2, parseInt(Math.log2(curIndex)));
     if (curIndex - lowerLine > lowerLine * 2 - 1 - curIndex) {
@@ -34,7 +34,7 @@ Heap.prototype.getByIndex = function(index) {
     return curNode.childLeft;
   }
   else if (curIndex === 3) {
-    return curNode.childLeft;
+    return curNode.childRight;
   }
   else {
     throw new Error('Error of reading tree');
@@ -64,3 +64,45 @@ Heap.prototype.add = function(priority, data) {
   }
 }
 
+Heap.prototype.getMax = function() {
+  let max = this.root.data;
+  let last = this.getByIndex(this.countNodes);
+  this.root.priority = last.priority;
+  this.root.data = last.data;
+  last.parent[this.countNodes % 2 ? 'childRight' : 'childLeft'] = null;
+  console.log(last)
+  let curNode = this.root;
+  let curMaxChild = curNode.childLeft ? curNode.childRight ?
+    curNode.childRight.priority > curNode.childLeft.priority ?
+    curNode.childLeft : curNode.childRight : curNode.childLeft : null;
+  while (curNode.childLeft && curMaxChild.priority < curNode.priority) {
+    let tempData = curNode.data;
+    let tempPriority = curNode.priority;
+    curNode.data = curMaxChild.data;
+    curNode.priority = curMaxChild.priority;
+    curMaxChild.data = tempData;
+    curMaxChild.priority = tempPriority;
+    curNode = curMaxChild;
+    curMaxChild = curNode.childLeft ? curNode.childRight ?
+    curNode.childRight.priority > curNode.childLeft.priority ?
+    curNode.childLeft : curNode.childRight : curNode.childLeft : null;
+  }
+  return max;
+}
+
+// Tests:
+
+// const f = new Heap();
+
+// f.add(20, 3);
+// f.add(10, 4);
+// f.add(23, 4);
+// f.add(12, 4);
+// f.add(8, 's')
+// // console.dir(f, {depth: null})
+// // console.log('MINMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM');
+// console.log(f.getMax());
+// console.dir(f, {depth: null})
+// // console.log(f.getByIndex(5))
+// console.log('asdadasdasd')
+// setTimeout(() => console.dir(f, {depth: null}), 5000)
