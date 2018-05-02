01cb816c9a0a9919d79049047c89f8faf4e72ca8
Nicita01 <nicitamac@gmail.com>
2018-05-02 03:07:58 +0300

    Fix binary-heap

--- A*/binary-heap.js
+++ A*/binary-heap.js
@@ -4,7 +4,7 @@ function Heap(...nodes) {
   this.root = null;
   this.last = null;
   this.countNodes = 0;
-  for (let curNodes of nodes) {
+  for (const curNodes of nodes) {
     this.add(curNodes[0], curNodes[1]);
   }
 }
@@ -24,25 +24,23 @@ Heap.prototype.getByIndex = function(index) {
   let curNode = this.root;
   let curIndex = index;
   while (curIndex !== 2 && curIndex !== 3) {
-    let lowerLine = Math.pow(2, parseInt(Math.log2(curIndex)));
+    const lowerLine = Math.pow(2, parseInt(Math.log2(curIndex)));
     if (curIndex - lowerLine > lowerLine * 2 - 1 - curIndex) {
       curNode = curNode.childRight;
-      curIndex = curIndex - lowerLine;
+      curIndex -= lowerLine;
     } else {
       curNode = curNode.childLeft;
-      curIndex = curIndex - lowerLine / 2;
+      curIndex -= lowerLine / 2;
     }
   }
   if (curIndex === 2) {
     return curNode.childLeft;
-  }
-  else if (curIndex === 3) {
+  } else if (curIndex === 3) {
     return curNode.childRight;
-  }
-  else {
+  } else {
     throw new Error('Error of reading tree');
   }
-}
+};
 
 Heap.prototype.add = function(priority, data) {
   if (!(this.countNodes)) {
@@ -50,54 +48,54 @@ Heap.prototype.add = function(priority, data) {
     this.countNodes++;
     return;
   }
-  let parentOfNew = this.getByIndex(Math.floor((this.countNodes + 1) / 2));
-  parentOfNew[this.countNodes % 2 ? 'childLeft' : 'childRight'] = new Node(priority, data);
-  this.countNodes++;
-  let curNode = this.getByIndex(this.countNodes);
+  const parentOfNew = this.getByIndex(Math.floor((this.countNodes + 1) / 2));
+  parentOfNew[this.countNodes % 2 ? 'childLeft' : 'childRight'] =
+    new Node(priority, data);
+  let curNode = this.getByIndex(++this.countNodes);
   curNode.parent = parentOfNew;
   while (curNode.parent && curNode.priority < curNode.parent.priority) {
-    let parent = curNode.parent;
-    let tempData = parent.data;
-    let tempPriority = parent.priority;
+    const parent = curNode.parent;
+    const tempData = parent.data;
+    const tempPriority = parent.priority;
     parent.data = curNode.data;
     parent.priority = curNode.priority;
     curNode.data = tempData;
     curNode.priority = tempPriority;
     curNode = curNode.parent;
   }
-}
+};
 
 Heap.prototype.get = function() {
   if (!(this.root)) return null;
   if (this.countNodes === 1) {
-    let max = this.root.data;
+    const max = this.root.data;
     this.root = null;
     this.countNodes--;
     return max;
   }
-  let max = this.root.data;
-  let last = this.getByIndex(this.countNodes);
+  const max = this.root.data;
+  const last = this.getByIndex(this.countNodes);
   this.root.priority = last.priority;
   this.root.data = last.data;
   last.parent[this.countNodes % 2 ? 'childRight' : 'childLeft'] = null;
   let curNode = this.root;
   let curMaxChild = curNode.childLeft ? curNode.childRight ?
     curNode.childRight.priority > curNode.childLeft.priority ?
-    curNode.childLeft : curNode.childRight : curNode.childLeft : null;
+      curNode.childLeft : curNode.childRight : curNode.childLeft : null;
   while (curNode.childLeft && curMaxChild.priority < curNode.priority) {
-    let tempData = curNode.data;
-    let tempPriority = curNode.priority;
+    const tempData = curNode.data;
+    const tempPriority = curNode.priority;
     curNode.data = curMaxChild.data;
     curNode.priority = curMaxChild.priority;
     curMaxChild.data = tempData;
     curMaxChild.priority = tempPriority;
     curNode = curMaxChild;
     curMaxChild = curNode.childLeft ? curNode.childRight ?
-    curNode.childRight.priority > curNode.childLeft.priority ?
-    curNode.childLeft : curNode.childRight : curNode.childLeft : null;
+      curNode.childRight.priority > curNode.childLeft.priority ?
+        curNode.childLeft : curNode.childRight : curNode.childLeft : null;
   }
-  this.last = this.getByIndex(--this.countNodes)
+  this.last = this.getByIndex(--this.countNodes);
   return max;
-}
+};
 
 module.exports = Heap;
